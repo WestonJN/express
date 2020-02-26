18fc84d476ec391eca09ae341d67058dfb445725
WestonJN <westonnyirenda@gmail.com>
2020-02-26 13:59:09 +0200

    App changes

--- src/app.js
+++ src/app.js
@@ -1,38 +1,48 @@
-var express = require('express');
-var path = require("path");
+const express = require('express');
+const path = require("path");
 
-var app = express();
+//createing app
+const app = express();
 
-var bodyParser = require('body-parser');
-var {addNewVisitor, createTable} = require("./db");
+// const bodyParser = require('body-parser');
+//declaring database
+const {addNewVisitor, createTable} = require("./db");
 
-var pug = require("pug")
+// const pug = require("pug");
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.use(express.static('public'));
+app.use('/new_visitor', express.static('public'));
 
 app.set('view engine', 'pug')
-app.set('./view', path.join(__dirname, './view'))
+// app.set('./views', path.join(__dirname, './views'))
+
+app.get('/', function (req,res){
+   res.render('index')
+})
 
 app.get('/new_visitor', function (req, res) {
    res.sendFile(path.join(__dirname + "/index.html" ));
 })
 
-app.post('/thank_you', function (req, res) {
+app.post('/new_visitor', async function (req, res) {
 
-   if (req.body)
-      throw new Error('The requested body cannot be empty')
+  let vname=req.body.vname;
+  let aname=req.body.aname;
+  let  age=req.body.age;
+  let date=req.body.date;
+  let time=req.body.date;
+  let comments=req.body.comments;
 
-      res.render("index", {
-         vname:req.body.vname
-      });
+   const visitor =await addNewVisitor(vname,aname,age,date,time,comments);
+
+     return res.render("success", { data: visitor[0] });
 })
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
+const server = app.listen(8081, function () {
+   const host = server.address().address
+   const port = server.address().port
    
    console.log("Example app listening at http://%s:%s", host, port)
 })
